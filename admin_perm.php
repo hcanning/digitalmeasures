a454beaae77dc3d3a95f174836bd1ce5009f0299
hcanning <46687863+hcanning@users.noreply.github.com>
2019-02-05 13:33:55 -0500

    Add files via upload

---
+++ admin_perm.php
@@ -0,0 +1,34 @@
+<?php
+$curl = curl_init();
+$user = "yourUsername";
+$password = "yourPassword";
+curl_setopt_array($curl, array(
+CURLOPT_URL =>
+'https://betawebservices.digitalmeasures.com/login/service/v4/SchemaData/INDIVIDUAL-ACTIVITIES-University/USERNAME:jDoe',
+CURLOPT_USERPWD => $user . ':' . $password,
+CURLOPT_ENCODING => 'gzip',
+CURLOPT_FOLLOWLOCATION => true,
+CURLOPT_RETURNTRANSFER => true));
+$responseData = curl_exec($curl);
+if(curl_errno($curl))
+{
+$errorMessage = curl_error($curl);
+// TODO: Handle cURL error
+}
+else
+{
+$statusCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+// TODO: Handle HTTP status code and response data
+//$movies = new SimpleXMLElement($xmlstr);
+$oXML = new SimpleXMLElement( $responseData );
+//header( 'Content-type: text/xml' );
+//echo $oXML->asXML();
+//echo $oXML->FNAME . "<br>";
+echo $oXML->Record->PCI[0]->FNAME;
+echo $oXML->Record->PCI[0]->BUILDING;
+echo $oXML->Record->ADMIN_PERM[0]->SRANK;
+
+}
+curl_close($curl);
+
+?>
